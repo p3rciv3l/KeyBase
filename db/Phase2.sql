3d920377d42da9de1ff94164e7228ab82e5192d7
Derrick Gan <gan.derrick@gmail.com>
2022-12-03 10:50:51 -0500

    updated bootstrap file

--- db/Phase2.sql
+++ db/Phase2.sql
@@ -1,24 +1,3 @@
--- This file is to bootstrap a database for the CS3200 project. 
-
--- Create a new database.  You can change the name later.  You'll
--- need this name in the FLASK API file(s),  the AppSmith 
--- data source creation.
-create database keybase;
-
--- Via the Docker Compose file, a special user called webapp will 
--- be created in MySQL. We are going to grant that user 
--- all privilages to the new database we just created. 
--- TODO: If you changed the name of the database above, you need 
--- to change it here too.
-grant all privileges on keybase.* to 'webapp'@'%';
-flush privileges;
-
--- Move into the database we just created.
--- TODO: If you changed the name of the database above, you need to
--- change it here too. 
-use keybase;
-
--- Put your DDL 
 CREATE TABLE GeneralUser
 (
     UserID           STRING PRIMARY KEY,
@@ -464,4 +443,3 @@ insert into BusinessFeatures (FeatureID, PasswordStrength, PasswordReuseWithinBu
 insert into BusinessFeatures (FeatureID, PasswordStrength, PasswordReuseWithinBusiness, PasswordSharingAbility, PasswordStrengthRelatedToInternalProducts, PasswordLeaked) values ('931-585', 'Very Strong', 0, 1, 'Weak', 1);
 insert into BusinessFeatures (FeatureID, PasswordStrength, PasswordReuseWithinBusiness, PasswordSharingAbility, PasswordStrengthRelatedToInternalProducts, PasswordLeaked) values ('944-171', 'Very Strong', 0, 0, 'Weak', 0);
 insert into BusinessFeatures (FeatureID, PasswordStrength, PasswordReuseWithinBusiness, PasswordSharingAbility, PasswordStrengthRelatedToInternalProducts, PasswordLeaked) values ('342-947', 'Very Strong', 1, 1, 'Very Strong', 1);
--- Add sample data. 
